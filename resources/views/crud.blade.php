b097bd4bd4b8a97bb2555c885610772a634d0b9c
Ahmadjaelani-20110127 <Ahmadjaelani011000@gmail.com>
2023-12-03 16:30:16 +0700

    Subbmission 3

---
+++ resources/views/crud.blade.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CRUD</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Data Peminjaman Buku Perpustakaan</h1>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Id</th>
+      <th scope="col">Nama Buku</th>
+      <th scope="col">Jumlah Buku</th>
+      <th scope="col">Kondisi Buku</th>
+      <th scope="col">Jumlah </th>
+      <th scope="col">Aksi</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Pencari Tuhan Jilid 1</td>
+      <td>8</td>
+      <td>Baik</td>
+      <td><button type="button" class="btn btn-success">Pinjam</button></td>
+      <td><button type="button" class="btn btn-danger">Pengembalian</button></td>
+      
+    <tr>
+      <th scope="row">2</th>
+      <td>Motivasi Membangun Usaha</td>
+      <td>4</td>
+      <td>sangat baik</td>
+      <td><button type="button" class="btn btn-success">Pinjam</button></td>
+      <td><button type="button" class="btn btn-danger">Pengembalian</button></td>
+    </tr>
+      <th scope="row">3</th>
+      <td>Yang Terlupakan</td>
+      <td>2</td>
+      <td>Cukup Baik</td>
+      <td><button type="button" class="btn btn-success">Pinjam</button></td>
+      <td><button type="button" class="btn btn-danger">Pengembalian</button></td>
+    <tr>
+   
+    </tr>
+  </tbody>
+</table>
+   
+   
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
